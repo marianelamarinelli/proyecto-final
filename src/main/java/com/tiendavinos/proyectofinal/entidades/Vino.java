a176d80b35f7b7d48e7607f40fdeed6e44736d00
Juani-Ortizz <65369113+Juani-Ortizz@users.noreply.github.com>
2020-07-13 20:13:04 -0300

    agregue get y set de la clase vino
    arregle el nombre del año

--- src/main/java/com/tiendavinos/proyectofinal/entidades/Vino.java
+++ src/main/java/com/tiendavinos/proyectofinal/entidades/Vino.java
@@ -25,5 +25,72 @@ public class Vino {
     private Foto foto;
     private Double precio;
     private List<Comentario> listadoComentario;
-    private String añoCosecha;
+    private String anioCosecha;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombreVino() {
+        return nombreVino;
+    }
+
+    public void setNombreVino(String nombreVino) {
+        this.nombreVino = nombreVino;
+    }
+
+    public Bodega getBodega() {
+        return bodega;
+    }
+
+    public void setBodega(Bodega bodega) {
+        this.bodega = bodega;
+    }
+
+    public Integer getCapacidadBotella() {
+        return capacidadBotella;
+    }
+
+    public void setCapacidadBotella(Integer capacidadBotella) {
+        this.capacidadBotella = capacidadBotella;
+    }
+
+    public Foto getFoto() {
+        return foto;
+    }
+
+    public void setFoto(Foto foto) {
+        this.foto = foto;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public List<Comentario> getListadoComentario() {
+        return listadoComentario;
+    }
+
+    public void setListadoComentario(List<Comentario> listadoComentario) {
+        this.listadoComentario = listadoComentario;
+    }
+
+    public String getAnioCosecha() {
+        return anioCosecha;
+    }
+
+    public void setAnioCosecha(String anioCosecha) {
+        this.anioCosecha = anioCosecha;
+    }
+    
+    
+    
 }
